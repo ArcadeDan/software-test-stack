63fef621eb8137f2c5a4ee50da7b4bf373ec4a6c
dan <lordaniel24@gmail.com>
2023-04-18 16:58:01 -0500

    auth

--- server.js
+++ server.js
@@ -63,7 +63,38 @@ app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/txt', express.static(__dirname + 'public/js'));
 
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
 
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+    console.log(users);
+});
+
+app.delete('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
 
 const pythonScript = spawn('python3', ['test.py', '2', '3']);
 app.listen(port, () => console.log(`Test app listening on port ${port}`));
